06f3baa3e41839435432abff751a54e98767aa38
anna-grinker <grinker.anna@gmail.com>
2023-11-29 21:00:12 +0200

    css selectors

---
+++ src/test/java/HomeWorks.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HomeWorks {
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("https://telranedu.web.app/login");
+    }
+
+    @Test
+    public void cssLocators(){
+        //by tag name
+        //by class
+        //by ID
+        //by attribute
+    }
+}
---
+++ src/test/java/Index.java
@@ -0,0 +1,51 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Index {
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.get("file:///F:/D/%D0%A3%D0%BD%D0%B8%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%82%D0%B5%D1%82%20-%20%D0%98%D0%B7%D1%80%D0%B0%D0%B8%D0%BB%D1%8C/Tel%20Ran/06%20Automation/02%20HTML,%20CSS/21.index.html");
+    }
+
+    @Test
+    public void cssLocators(){
+        //by tag name
+        WebElement e1 = wd.findElement(By.tagName("button"));//selenium library
+        WebElement element = wd.findElement(By.cssSelector("button"));
+
+        List<WebElement> list = wd.findElements(By.tagName("a"));
+        int i = list.size();
+        WebElement el3 = list.get(2);
+
+        //by class
+        WebElement divcontainer = wd.findElement(By.className("container"));
+        WebElement divcontainer1 = wd.findElement(By.cssSelector(".container"));//!!! dot because it is a class
+
+        List<WebElement> navlist = wd.findElements(By.className("nav-item"));
+        List<WebElement> navlist1 = wd.findElements(By.cssSelector(".nav-item"));
+
+        //by id
+        WebElement nav = wd.findElement(By.id("nav"));
+        WebElement nav1 = wd.findElement(By.cssSelector("#nav"));//!!! # because it is an ID
+
+        //by attributes such as [href], [name] etc.
+        //[href='#item'] - the value of attribute in Selenium devTool.
+        WebElement inputEmail = wd.findElement(By.cssSelector("[placeholder='Type your name']"));
+        WebElement a2 = wd.findElement(By.cssSelector("[href='#item2']"));
+
+        WebElement input1 = wd.findElement(By.cssSelector("[name='name']"));
+        WebElement inputS = wd.findElement(By.name("name"));
+
+    }
+
+}
